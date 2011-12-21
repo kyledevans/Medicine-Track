405d401f98296673168facbf5d566a208ecff75a
Kyle Evans <kyledevans@gmail.com>
2011-12-21 09:18:45 -0600

    Added a foreign key command. 	new file:   foreign_keys.sql

---
+++ foreign_keys.sql
@@ -0,0 +1,7 @@
+select 
+    concat(table_name, '.', column_name) as 'foreign key',  
+    concat(referenced_table_name, '.', referenced_column_name) as 'references'
+from
+    information_schema.key_column_usage
+where
+    referenced_table_name is not null;
