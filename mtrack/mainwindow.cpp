32771a82da5c5ecc9187df000072786ba245041a
Kyle Evans <kyledevans@gmail.com>
2011-12-22 16:47:08 -0600

    Basic connection to MySQL. 	modified:   mtrack/main.cpp 	modified:   mtrack/mainwindow.cpp 	modified:   mtrack/mainwindow.ui

--- mtrack/mainwindow.cpp
+++ mtrack/mainwindow.cpp
@@ -1,3 +1,8 @@
+#include <QTableView>
+#include <QSqlDatabase>
+#include <QSqlQueryModel>
+#include <QTableView>
+
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
@@ -5,7 +10,27 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+	ui->setupUi(this);
+
+	// SQL test code
+	QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
+	db.setHostName("192.168.1.99");
+	db.setDatabaseName("epc");
+	db.setUserName("kyle");
+	db.setPassword("kobalt");
+	db.open();
+
+	QSqlQueryModel *model = new QSqlQueryModel;
+	model->setQuery("SELECT first, last FROM patients;");
+	model->setHeaderData(0, Qt::Horizontal, tr("id"));
+	model->setHeaderData(1, Qt::Horizontal, tr("first"));
+	//model->setHeaderData(2, Qt::Horizontal, tr("last"));
+
+	QTableView *view = new QTableView(ui->centralWidget);
+	view->setModel(model);
+	view->show();
+
+	//delete model;
 }
 
 MainWindow::~MainWindow()
