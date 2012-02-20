f0087f88b1848fe87ed36ace32a72edbab0841bf
Kyle Evans <kyledevans@gmail.com>
2012-02-20 16:13:24 -0600

    Small work on test data. 	modified:   test_data/drugs.sql 	modified:   test_data/patients.sql 	modified:   test_data/pharmacists.sql 	modified:   test_data/prescribers.sql

--- test_data/pharmacists.sql
+++ test_data/pharmacists.sql
@@ -1,8 +1,8 @@
 ﻿INSERT INTO pharmacists (id, last, first, initials, active) VALUES(1, 'Simpson', 'Homer', 'HS', 0);
-INSERT INTO pharmacists (id, last, first, initials, active) VALUES(2, 'Fry', 'Philip', 'PJF', 1);
+INSERT INTO pharmacists (id, last, first, initials, active) VALUES(18, 'Fry', 'Philip', 'PJF', 1);
 INSERT INTO pharmacists (id, last, first, initials, active) VALUES(3, 'Rodriguez', 'Bender', 'BR', 1);
 INSERT INTO pharmacists (id, last, first, initials, active) VALUES(4, 'Simpson', 'Maggie', 'MS', 0);
 INSERT INTO pharmacists (id, last, first, initials, active) VALUES(5, 'Wong', 'Amy', 'AW', 1);
-INSERT INTO pharmacists (id, last, first, initials, active) VALUES(6, 'Leela', 'Turanga', 'TL', 1);
-INSERT INTO pharmacists (id, last, first, initials, active) VALUES(7, 'Nahasapeemapetilon', 'Apu', 'AN', 1);
-ALTER TABLE pharmacists AUTO_INCREMENT=8;
+INSERT INTO pharmacists (id, last, first, initials, active) VALUES(55, 'Leela', 'Turanga', 'TL', 1);
+INSERT INTO pharmacists (id, last, first, initials, active) VALUES(56, 'Nahasapeemapetilon', 'Apu', 'AN', 1);
+ALTER TABLE pharmacists AUTO_INCREMENT=57;
